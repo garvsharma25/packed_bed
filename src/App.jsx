1d40ce7ab67af55cdfe40d9b900e1232dd11b086
garv sharma <sharmagarv51@gmail.com>
2025-05-24 11:20:16 +0530

    packed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,52 @@
 import pump from "./assets/pump.png";
 import sphere from "./assets/sphere.png";
 import tube from "./assets/tube.png";
-let height =70 ;
- const pumpOn=()=>{
-  alert("on");
-  height=height + 5;
- };
+import pipe from './assets/pipe.png';
+import handle from './assets/handle.png';
+import manometer from './assets/manometer.png'
+import packed from './assets/packed.png'
+import * as motion from "motion/react-client"
+// const [height, setHeight] = useState(70);
+// const pumpOn = () => {
+//   alert("on");
+//   setHeight((prev) => {
+//     prev + 5;
+//   });
+// };
+
 //bg-[url(./assets/image.png)]
 function App() {
   return (
-    <div className=" w-full h-screen">
-      <div className="flex h-1/2 w-full">
-        <div className=" w-1/2"></div>
+    <div className=" w-[900px] justify-center pl-34 align-middle flex-col h-[900px]"> 
+      
+      <div className="flex h-3/4 w-full">
+      <motion.div  whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 0.8 }}  className=" relative w-1/3">
+      <img src={packed} className="h-full w-1/2 mx-auto "/>
+      
+      </motion.div>
+       <div className="relative w-1/3">
+       <img src={manometer} className="top-3 h-3/4 w-full  absolute"/>
+       </div>
+ <div className=" w-1/3">
+ <div className="h-3/4 flex relative  justify-center ">
+ 
+  <img src={tube} alt="pump" className="h-120 w-100 absolute " />
 
-        
-
-        <div className=" w-1/2 static flex  justify-center  ">
-        <img src={tube} alt="pump" className="h-[50vh] w-[50vh] absolute " />
-
-        <img src={sphere} alt="pump"  className={`bottom-[${height}%] h-[5vh] w-[5vh] absolute z-10`}/>
-        </div>
-
+        <img src={sphere} alt="pump"  className={` h-10 w-10 absolute top-4 z-10`}/>
+ 
+ </div>
+ 
+ <div className="h-1/4 relative flex justify-center  ">
+ <img src={pipe} className="absolute w-full h-full "/>
+ <img src={handle }  className="h-1/2  absolute top-10 z-20  "/>
+ <h1 className="bottom-0.5 z-50 text-red-600 absolute">by pass value </h1>
+ </div>
+ </div>
       </div>
 
-      <div className="w-full h-1/4 flex  justify-center" onClick={pumpOn}>
-        <img src={pump} alt="pump" className="h-[50vh] w-[50vh] " />
+      <div className="w-full h-1/4 flex    " >
+        <img src={pump} alt="pump" className="h-80 w-80 " />
       </div>
     </div>
   );
---
+++ src/Object.js
@@ -0,0 +1,20 @@
+
+const object =[
+{id:'1',title:'click on the motor button to switch the motor on'},
+{id:'2',title:'click right and left side of the bypass value to better control the flow of water '}
+,{id:'3',title:'the rate of flow in the tower can be controlled by a valve in the inlet pipe'}
+,{id:'4',title:'The inlet valve is maintained in such a way that level of water in manometric tubesare equal'},
+,{id:'5',title:'The water is allowed to flow through the packing in the tower. All the air pockets in	the tower and in the manometer are removed'}
+,{id:'6 ',title:'The distance between the pressure taps is noted as height of the tower(L).' },
+{id:'6',title:'The outlet valve is opened'}
+,{id:'7',title:'Time taken to collect the water is noted.'},
+{id:'8',title:'The flow rate is calculated from the volume of water collected or rotameter coonected to setup'}
+
+]
+
+
+
+
+
+ 
+
